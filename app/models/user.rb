0a51ef407f4689f77664b366570488d8089b164b
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-04 11:34:09 +0300

    Set user role using enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          :confirmable
+
+  enum role: [:customer, :owner, :Admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
