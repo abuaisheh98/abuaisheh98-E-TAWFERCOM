44ab8e4852658cb69fd9caf448db18a7016fa04a
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-04 10:46:17 +0300

    Authintecation Using devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_job = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp@gmail.com',
+  port: 465,
+  domain: 'gmail.com',
+  user_name: ENV['laithabuaisheh9@gmail.com'],
+  password: ENV['000000000'],
+  authentication: 'plain',
+  :ssl => true ,
+  :tsl => true ,
+  enable_starttls_auto: true
+}
