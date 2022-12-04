e070484477c691a0fb25cfa902199dda27f6df8e
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-04 16:34:17 +0300

    refactor validation code in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,11 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :production_dste, comparison: {greater_than: :expiry_date}
-  validate :expiration_date_cannot_be_in_th_past,
-           :price_cannot_be_less_than_or_equal_zero,
-           :stock_quantity_cannot_be_less_than_zero
+  validate :expiration_date_cannot_be_in_th_past
+           # :price_cannot_be_less_than_or_equal_zero,
+           # :stock_quantity_cannot_be_less_than_zero
+  validates :price, numericality: {greater_than: 0, message: "Can't be less than or equal zero"}
+  validates :stock_quantity, numericality: {greater_than_or_equal_to: 0, message: "Can't be less than zero"}
 
   def expiration_date_cannot_be_in_th_past
     if expiry_date.present? and expiry_date < Date.today
@@ -13,15 +15,16 @@ class Product < ApplicationRecord
     end
   end
 
-  def price_cannot_be_less_than_or_equal_zero
-    if price.present? and price <= 0
-      errors.add(:price, "Can't be less than or equal zero")
-    end
-  end
+  # def price_cannot_be_less_than_or_equal_zero
+  #   if price.present? and price <= 0
+  #     errors.add(:price, "Can't be less than or equal zero")
+  #   end
+  # end
 
-  def stock_quantity_cannot_be_less_than_zero
-    if stock_quantity.present? and stock_quantity < 0
-      errors.add(:stock_quantity, "Can't be less than zero")
-    end
-  end
+  # def stock_quantity_cannot_be_less_than_zero
+  #   if stock_quantity.present? and stock_quantity < 0
+  #     errors.add(:stock_quantity, "Can't be less than zero")
+  #   end
+  # end
+
 end
