9fcb38aed22e26f4d576bbc0969fe18875fa12c5
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-05 15:32:04 +0300

    display product & category in home page (test)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    @products = Product.first(3)
+    @featured_products = Product.order(price: :asc).first(10)
+    @offer = Product.order(stock_quantity: :asc).first(2)
   end
+
+
+
 end
