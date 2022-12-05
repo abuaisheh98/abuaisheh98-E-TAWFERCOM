49cecff5bcd3ae80ae93504b6e161e53153de47d
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-05 22:42:50 +0300

    work with manifest.js, edit css & js file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,15 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
+//= link_tree ../javascripts .js
 //= link_tree ../../../vendor/javascript .js
+//= link main.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link application.js
+//= require ../javascripts/main
+//= require easing/easing.min
+//= require owl.carousel.js
+//= require owl.carousel.min
+
+
