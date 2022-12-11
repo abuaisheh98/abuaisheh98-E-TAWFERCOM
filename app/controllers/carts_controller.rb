60eaacf9faecdac25153fad73823f87920f47410
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-11 10:41:50 +0300

    create order if current_user checked out

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,20 +2,35 @@ class CartsController < ApplicationController
   before_action :redirect_by_role, if: -> { current_user.present? }
   def index
     item_view = Struct.new(:product, :quantity)
-    if session["products"].nil?
+    unless session["products"].present?
       redirect_to controller: :static_pages, action: :home
     else
       @cart = []
+      @sub_total = 0
         session["products"].each do |item|
           instance = item_view.new(Product.find(item["product_id"]),item["quantity"])
+          @sub_total += instance["product"].price * instance["quantity"]
           @cart << instance
+          session["checkout"] = @cart
         end
     end
   end
 
   def checkout
     if current_user.present?
-      redirect_to root_path
+      if current_user.role == 'customer'
+        order = Order.new
+        order.status = "paid"
+        order.quantity = 0
+        session["checkout"].each do |item|
+          order.quantity += item["quantity"]
+        end
+        order.order_date = Date.today
+        order.user_id = current_user.id
+        order.save!
+        session["checkout"] = nil
+        redirect_to controller: :static_pages, action: :home
+      end
     else
       redirect_to new_user_session_path
     end
