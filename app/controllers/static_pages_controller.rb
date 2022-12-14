82d6bd265b7942551008846294768b76ccab5762
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-14 12:24:14 +0300

    Update project

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,10 @@ class StaticPagesController < ApplicationController
   before_action :redirect_by_role, if: -> { current_user.present? }
   def home
     @products = Product.first(3)
-    @featured_products = Product.order(price: :asc).first(10)
+    @featured_products = Product.low_price.order(price: :asc).first(12)
     @offer = Product.order(stock_quantity: :asc).first(2)
-    @x = Product.all
+    @low_price = Product.low_price
+    @high_price = Product.high_price
   end
 
   def add_to_cart
@@ -23,6 +24,28 @@ class StaticPagesController < ApplicationController
       session["products"] = cart
     end
     redirect_back fallback_location: root_path
+  end
+
+  def search
+    if params[:search].blank?
+      redirect_to action: :home
+    else
+      @result = Product.by_name(params[:search].downcase)[0]
+    end
+  end
+
+  def products_by_category
+    @category_name = Category.find(params[:id]).name
+    @products_by_category = (Category.find(params[:id])).products
+  end
+
+  def display_store
+    @stores = Store.all
+  end
+
+  def products_by_stores
+    @store_name = Store.find(params[:id]).name
+    @products_by_store = (Store.find(params[:id])).products
   end
 
   def remove_from_cart
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,10 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than: 0, message: "Can't be less than or equal zero"}
   validates :stock_quantity, numericality: {greater_than_or_equal_to: 0, message: "Can't be less than zero"}
 
+  scope :low_price, -> {where("price < ?",100)}
+  scope :high_price, -> {where("price > ?",200)}
+  scope :by_name, ->(name) {where("name = ?",name)}
+
   def expiration_date_cannot_be_in_the_past
     if expiry_date.present? and expiry_date < Date.today
       errors.add(:expiry_date, "Can't be in the past")
