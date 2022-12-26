ac724f84455ddb54cdb1a1d2b09cc2afb2ccc9d8
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-27 00:29:00 +0300

    import products from csv file

--- app/services/csv_import_products_service.rb
+++ app/services/csv_import_products_service.rb
@@ -4,14 +4,7 @@ class CsvImportProductsService
     file = File.open(file)
     csv = CSV.parse(file, headers: true)
     csv.each do |row|
-      result = Product.where(:name => row["name"], :store_id => row["store_id"]).first_or_create
-      result.price = row["price"].to_f
-      result.stock_quantity = row["stock_quantity"].to_i
-      result.description = row["description"]
-      result.production_dste = row["production_dste"]
-      result.expiry_date = row["expiry_date"]
-      result.image = row["image"]
-      result.save
+      Product.upsert({ id: row["id"], name: row["name"], store_id: row["store_id"], price: row["price"].to_f, stock_quantity: row["stock_quantity"].to_i, description: row["description"], image: row["image"] })
       end
   end
 end
